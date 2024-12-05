ebca1706bcb7994241ee1ea984c64e8d1124ae66
Emie-Trialer <emiedonmukumo@gmail.com>
2024-12-05 20:27:10 +0100

    update

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ import investorRoute from './routes/investorRoute.js';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import cors from 'cors';
+import { fileURLToPath } from 'url';
+
+import path from 'path';
+
+
 
 dotenv.config();
 connectDB();
@@ -35,8 +40,17 @@ const options = {
 // Create swagger spec
 const swaggerSpec = swaggerJsdoc(options);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
 app.use(cors());
+app.use('/swagger-static', express.static(path.join(__dirname, 'node_modules', 'swagger-ui-dist')));
+app.use(
+    '/docs',
+    swaggerUi.serveFiles(null, { swaggerOptions: { url: '/swagger-static/swagger.json' } }),
+    swaggerUi.setup(null, { swaggerOptions: { url: '/swagger-static/swagger.json' } })
+);
 
 // Serve swagger docs
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
