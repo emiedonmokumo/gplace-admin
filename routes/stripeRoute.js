4ab29586da6f51b32fa1b8772cb7d3163cb5dac7
Emie-Trialer <emiedonmukumo@gmail.com>
2024-12-07 17:09:50 +0100

    update

--- routes/stripeRoute.js
+++ routes/stripeRoute.js
@@ -4,6 +4,6 @@ import bodyParser from 'body-parser';
 const router = express.Router()
 
 // Event Route
-router.post('/webhook', bodyParser.raw({ type: 'application/json' }), handleStripeWebhook);
+router.get('/webhook', bodyParser.raw({ type: 'application/json' }), handleStripeWebhook);
 
 export default router
